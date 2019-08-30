57b1df1a7a0ad1e773af4e0f8840056b00e62823
Chiyang Wan <chiyang10000@gmail.com>
2019-08-30 14:18:14 +0800

    Update CREATE DATABASE on PostgreSQL

--- planchecker.sql
+++ planchecker.sql
@@ -1,11 +1,12 @@
 -- Main table to store plans
+DROP TABLE IF EXISTS plans;
 CREATE TABLE plans (
-    id          INT NOT NULL AUTO_INCREMENT,
+    id          SERIAL NOT NULL,
     ref         VARCHAR(16) NOT NULL,
     plantext    TEXT NOT NULL,
-    created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (ref)
 );
 
 -- Add unique index on Ref field
-ALTER TABLE `plans` ADD UNIQUE INDEX `unique_index_ref` (`ref`);
+-- ALTER TABLE `plans` ADD UNIQUE INDEX `unique_index_ref` (`ref`);
